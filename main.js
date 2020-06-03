9e14d6333bc4af551c26cc02bf2ba6254abdb7a1
Phillip Miret <45930012+Phillip-Miret@users.noreply.github.com>
2020-06-03 13:19:21 -0400

    made a particle

--- main.js
+++ main.js
@@ -5,7 +5,7 @@ const ctx = canvas.getContext("2d");
 canvas.width = 1920;
 canvas.height = 1200;
 
-let radius = 5;
+let radius = 20;
 
 let v1 = new PVector(5, 5)
 let v2 = PVector.fromAngle(0);// new PVector(-10, -1)
@@ -18,21 +18,20 @@ let v3 = PVector.random2d();
 
 
 console.log(v2);
-console.log(v2.angleBetween(v1));
 
 
 
 
+let p1 = new particle(canvas.width/2, canvas.height/2, 0,0);
 
 
 
 setInterval(draw, 20);
 
 
-function particle(x, y, dx, dy){
-    
-    let location = new PVector(x, y);
-    let velocity = new PVector(dx, dy);
+function particle(x, y, dx, dy){   
+    this.location = new PVector(x, y);
+    this.velocity = new PVector(dx, dy);
     
 }
 
@@ -43,15 +42,14 @@ function particle(x, y, dx, dy){
 function drawParticle(particle){
 
     ctx.beginPath();
-    ctx.arc(particle.x, particle.y, radius, 0, 2*Math.PI);
+    ctx.arc(particle.location.x, particle.location.y, radius, 0, 2*Math.PI);
     ctx.fill();
-    particle.x += particle.dx;
-    particle.y += particle.dy;
+    
 
-    if(particle.x >= canvas.width || particle.x <= 0)
+    if(particle.location.x >= canvas.width || particle.location.x <= 0)
         particle.dx *= -1;
 
-     if(particle.y >= canvas.height || particle.y <= 0)
+     if(particle.location.y >= canvas.height || particle.location.y <= 0)
         particle.dy *= -1;
 
  
@@ -61,18 +59,18 @@ function drawParticle(particle){
 
 function draw(){
     ctx.clearRect(0, 0, canvas.width, canvas.height);
-    // drawParticle(p1);
+     drawParticle(p1);
 
-    ctx.beginPath();
-    ctx.arc(canvas.width/2, canvas.height/2, 20, 0, 2*Math.PI);
-    ctx.fill();
-    ctx.lineWidth = 10;
-    ctx.moveTo(canvas.width/2, canvas.height/2);
-    ctx.lineTo(canvas.width/2 + 20*v2.x, canvas.height/2 + 20*v2.y);
-    ctx.stroke();
+//     ctx.beginPath();
+//     ctx.arc(canvas.width/2, canvas.height/2, 20, 0, 2*Math.PI);
+//     ctx.fill();
+//     ctx.lineWidth = 10;
+//     ctx.moveTo(canvas.width/2, canvas.height/2);
+//     ctx.lineTo(canvas.width/2 + 20*v2.x, canvas.height/2 + 20*v2.y);
+//     ctx.stroke();
 
-    console.log(v2.heading());
-   v2.rotate(1);
+//     console.log(v2.angleBetween(v1));
+//    v2.rotate(1);
 
    
 }
