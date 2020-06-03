ca8f7fdc618bf4418f382c5c2a2edae14d1458b7
Phillip Miret <45930012+Phillip-Miret@users.noreply.github.com>
2020-06-03 10:26:34 -0400

    Update PVector.js

--- PVector.js
+++ PVector.js
@@ -14,6 +14,10 @@ class PVector{
         this.x = nx;
         this.y = ny;
     }
+    random2d(){
+        
+
+    }
 
     mag(){
         return Math.sqrt(Math.pow(this.x, 2) + Math.pow(this.y, 2));
@@ -28,5 +32,37 @@ class PVector{
         let tempVec = new PVector( this.x - v2.x, this.y - v2.y);
         return tempVec;
     }
+
+    mult(scal){
+        let tempVec = new PVector( this.x*scal, this.y*scal);
+        return tempVec
+    }
+
+    div(scal){
+        let tempVec = new PVector( this.x/scal, this.y/scal);
+        return tempVec
+    }
+
 }
+
+// get() return the x and y values in an array
+// set()	Set the components of the vector
+// random2D()	Make a new 2D unit vector with a random direction.
+// fromAngle()	Make a new 2D unit vector from an angle
+// copy()	Get a copy of the vector
+// mag()	Calculate the magnitude of the vector
+// add()	Adds x, y, and z components to a vector, one vector to another, or two independent vectors
+// sub()	Subtract x, y, and z components from a vector, one vector from another, or two independent vectors
+// mult()	Multiply a vector by a scalar
+// div()	Divide a vector by a scalar
+// dist()	Calculate the distance between two points
+// dot()	Calculate the dot product of two vectors
+// normalize()	Normalize the vector to a length of 1
+// limit()	Limit the magnitude of the vector
+// setMag()	Set the magnitude of the vector
+// heading()	Calculate the angle of rotation for this vector
+// rotate()	Rotate the vector by an angle (2D only)
+// lerp()	Linear interpolate the vector to another vector
+// angleBetween()	Calculate and return the angle between two vectors
+
 
