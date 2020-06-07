358421f73663b08fd88d89ef58b2bc3d2881d5f2
Phillip Miret <45930012+Phillip-Miret@users.noreply.github.com>
2020-06-06 21:09:46 -0400

    made an actual fluid sim, but it glitches out

---
+++ fluid.js
@@ -0,0 +1,228 @@
+const N = 64;
+const scale = 10;
+const iterations = 4;
+
+function IX (x, y){
+    return x + (y * N);
+}
+
+class fluid {
+
+    constructor( dt, diffusion, viscosity){
+        this.size = N
+        this.dt = dt;
+        this.diff = diffusion;
+        this.visc = viscosity;
+        
+        this.s = new Array(N*N).fill(0); 
+        this.density = new Array(N*N).fill(0);
+        
+       
+        
+        this.Vx = new Array(N*N).fill(0);
+        this.Vy = new Array(N*N).fill(0);
+        
+        this.Vx0 = new Array(N*N).fill(0);
+        this.Vy0 = new Array(N*N).fill(0);     
+    }
+
+    addDensity(x, y, amount){
+        let index = IX(x,y);
+        console.log(index);
+
+        this.density[index] += amount;
+    }
+
+    addVelocity(x, y, amountX, amountY){
+        let index = IX(x,y);
+        this.Vx[index] += amountX;
+        this.Vy[index] += amountY;
+
+    } 
+
+    displayDensity(){
+        for(let i = 0; i < N; i++){
+            for(let j = 0; j < N; j++){
+                let x = i*scale;
+                let y = j*scale;
+                let d = this.density[IX(i, j)];
+
+                ctx.beginPath();
+               ctx.globalAlpha = d;
+                ctx.rect(x, y, scale, scale);
+                ctx.fill();
+                ctx.closePath();
+            }
+        }
+    }
+
+    diffuse(b, xArr, x0Arr, diff, dt){
+        let a = dt * diff * (N - 2) * (N - 2);
+        this.lin_solve(b, xArr, x0Arr, a, 1 + 6 * a);
+
+    }
+    project(velocXArr, velocYArr, pArr, divArr){
+    
+        for (let j = 1; j < N - 1; j++) {
+            for (let i = 1; i < N - 1; i++) {
+                divArr[IX(i, j)] = -0.5*( // there was an f here
+                         velocXArr[IX(i+1, j )]
+                        -velocXArr[IX(i-1, j )]
+                        +velocYArr[IX(i  , j+1)]
+                        -velocYArr[IX(i  , j-1 )]
+                       
+                    )/N;
+                pArr[IX(i, j)] = 0;
+            }
+        }
+    
+    this.set_bnd(0, divArr); 
+    this.set_bnd(0, pArr);
+    this.lin_solve(0, pArr, divArr, 1, 6);
+    
+        for (let j = 1; j < N - 1; j++) {
+            for (let i = 1; i < N - 1; i++) {
+                velocXArr[IX(i, j)] -= 0.5 * (  pArr[IX(i+1, j)]
+                                                -pArr[IX(i-1, j)]) * N;
+                velocYArr[IX(i, j)] -= 0.5 * (  pArr[IX(i, j+1)]
+                                                -pArr[IX(i, j-1)]) * N;
+             
+        }
+    }
+    this.set_bnd(1, velocXArr);
+    this.set_bnd(2, velocYArr);
+   
+}
+advect(b, dArr, d0Arr,  velocXArr, velocYArr, dt)
+{
+    let i0, i1, j0, j1;
+    
+    let dtx = dt * (N - 2);
+    let dty = dt * (N - 2);
+   
+    
+    let s0, s1, t0, t1;
+    let tmp1, tmp2, x, y;
+    
+    let Nfloat = N;
+    let ifloat, jfloat;
+    let i, j;
+    
+  
+        for(j = 1, jfloat = 1; j < N - 1; j++, jfloat++) { 
+            for(i = 1, ifloat = 1; i < N - 1; i++, ifloat++) {
+                tmp1 = dtx * velocXArr[IX(i, j)];
+                tmp2 = dty * velocYArr[IX(i, j)];
+               
+                x    = ifloat - tmp1; 
+                y    = jfloat - tmp2;
+             
+                
+                if(x < 0.5) x = 0.5; 
+                if(x > Nfloat + 0.5) x = Nfloat + 0.5; 
+                i0 = Math.floor(x); 
+                i1 = i0 + 1.0;
+                if(y < 0.5) y = 0.5; 
+                if(y > Nfloat + 0.5) y = Nfloat + 0.5; 
+                j0 = Math.floor(y);
+                j1 = j0 + 1.0; 
+               
+                s1 = x - i0; 
+                s0 = 1.0 - s1; 
+                t1 = y - j0; 
+                t0 = 1.0 - t1;
+                
+                
+                let i0i = i0;
+                let i1i = i1;
+                let j0i = j0;
+                let j1i = j1;
+               
+                
+                dArr[IX(i, j)] = 
+                
+                    s0 * ( t0 *  d0Arr[IX(i0i, j0i)] + t1 *  d0Arr[IX(i0i, j1i)]) +
+                   s1 * ( t0 *  d0Arr[IX(i1i, j0i)] +  t1 *  d0Arr[IX(i1i, j1i)]);
+            }
+        }
+    
+        this.set_bnd(b, dArr);
+}
+
+lin_solve(b, x, x0, a, c){
+   let cRecip = 1.0 / c;
+    for (let k = 0; k < iterations; k++) { 
+        for (let j = 1; j < N - 1; j++) {
+            for (let i = 1; i < N - 1; i++) {
+                x[IX(i, j)] =
+                    (x0[IX(i, j)]
+                        + a*(x[IX(i+1, j )]
+                                +x[IX(i-1, j)]
+                                +x[IX(i  , j+1)]
+                                +x[IX(i  , j-1)]
+                             
+                         )) * cRecip;
+                }
+            }
+        
+            this.set_bnd(b, x);
+    }
+}
+
+
+set_bnd(b, xArr){
+  
+        for(let i = 1; i < N - 1; i++) {
+            xArr[IX(i, 0 )] = b == 2 ? -xArr[IX(i, 1 )] : xArr[IX(i, 1 )];
+            xArr[IX(i, N-1)] = b == 2 ? -xArr[IX(i, N-2)] : xArr[IX(i, N-2)];
+        }
+    
+    
+        for(let j = 1; j < N - 1; j++) {
+            xArr[IX(0  , j)] = b == 1 ? -xArr[IX(1  , j)] : xArr[IX(1  , j)];
+            xArr[IX(N-1, j)] = b == 1 ? -xArr[IX(N-2, j)] : xArr[IX(N-2, j)];
+        }
+    
+    
+    xArr[IX(0, 0)]       = 0.5 * (xArr[IX(1, 0)]
+                                  + xArr[IX(0, 1)]);
+                                 
+    xArr[IX(0, N-1)]     = 0.5 * (xArr[IX(1, N-1)]
+                                  + xArr[IX(0, N-2)]);
+                                  
+    xArr[IX(N-1, 0)]     = 0.5 * (xArr[IX(N-2, 0)]
+                                  + xArr[IX(N-1, 1)]);
+                                 
+    xArr[IX(N-1, N-1)]   = 0.5 * (xArr[IX(N-2, N-1)]
+                                  + xArr[IX(N-1, N-2)]);
+                          
+    }
+
+    step(){
+  
+    let visc     = this.visc;
+    let diff     = this.diff;
+    let dt       = this.dt;
+    let Vx     = this.Vx; // everything after this is an array;
+    let Vy      = this.Vy;
+    let Vx0     = this.Vx0;
+    let Vy0     = this.Vy0;
+    let s       = this.s;
+    let density = this.density;
+    
+    this.diffuse(1, Vx0, Vx, visc, dt);
+    this.diffuse(2, Vy0, Vy, visc, dt);
+ 
+   this.project(Vx0, Vy0, Vx, Vy);
+    
+   this.advect(1, Vx, Vx0, Vx0, Vy0, dt);
+   this.advect(2, Vy, Vy0, Vx0, Vy0, dt);
+  
+    
+   this.project(Vx, Vy, Vx0, Vy0);
+    
+   this.diffuse(0, s, density, diff, dt);
+   this.advect(0, density, s, Vx, Vy, dt);
+    }
+}
+
---
+++ particleFluidMain.js
@@ -0,0 +1,82 @@
+
+const canvas =document.getElementById("Canvas");
+const ctx = canvas.getContext("2d");
+
+canvas.width = 1920;
+canvas.height = 1200;
+
+let mouseDown = false;
+
+canvas.addEventListener("mousemove", (e) => {
+    if(mouseDown){
+        pArr.push(new particle(e.clientX - (window.innerWidth - canvas.width)/2 ,e.clientY - (window.innerHeight - canvas.height)/2, (Math.random() * 5 - 2.5), (Math.random() * 5- 2.5), 0, 0, 1));
+    }
+});
+
+canvas.addEventListener("mousedown",(e) => {
+     mouseDown = true;
+});
+canvas.addEventListener("mouseup",(e) => {
+    mouseDown = false
+});
+
+
+
+let radius = 5;
+
+let pArr = new Array(10); 
+
+for(let i = 0; i < pArr.length; i++){
+    pArr[i] = new particle((Math.random()* (canvas.width - radius - 1)) + (radius+ 1), (Math.random()* (canvas.height - radius - 1)) + (radius+ 1), 0,0, 0,0,1);
+}
+
+
+let f1 = new PVector(0,0);
+let gravity = new PVector(0,0.5);
+
+
+let MuF = 0.4;
+
+
+
+
+setInterval(draw, 20);
+
+
+
+
+function drawAll(p){
+    for(let i = 0; i < pArr.length; i++){
+        if(pArr[i]===p)
+            continue;
+        else if(p.collision(pArr[i])){
+            console.log("hit");
+            p.collide(pArr[i]);
+        }
+    }
+
+    p.drawParticle();
+    p.applyForce(f1);
+    tempGrav = gravity.mult(p.mass)  
+    p.applyForce(tempGrav);
+    p.applyFriction(MuF); 
+
+   // gravity.rotate(0.0002)
+
+    // t++;
+    // if (t > 600){
+    //     gravity.rotate(90);
+    //     t = 0;
+    // }
+   
+} 
+
+
+
+function draw(){
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    pArr.forEach(drawAll);
+
+    console.log(mouseDown);
+
+}
